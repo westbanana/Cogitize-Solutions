c3f4549374e12fe57a28da9d83fb438a38a3cde0
westbanana <93942346+westbanana@users.noreply.github.com>
2023-11-24 21:06:12 +0200

    Created Input component;

---
+++ src/shared/Input/ui/Input.tsx
@@ -0,0 +1,29 @@
+import cls from "./Input.module.scss";
+import {ChangeEvent, FC, useState} from "react";
+import {number} from "prop-types";
+import {set} from "immutable";
+interface InputProps {
+  placeholder?: string,
+  defaultValue?: string,
+}
+
+export const Input: FC<InputProps> = ({defaultValue, placeholder}) => {
+  const [value, setValue] = useState<string>(defaultValue || '')
+  
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const target = e.target
+    if (target) {
+      setValue(target.value)
+    }
+  }
+  
+  return (
+    <input
+      className={cls.Input}
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      onChange={onChangeHandler}
+    />
+  );
+};
