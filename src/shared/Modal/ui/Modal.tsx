7b09d4b5fb76b73983f34f06573c8894196f8a48
westbanana <93942346+westbanana@users.noreply.github.com>
2023-11-25 15:00:12 +0200

    Updated Modal;

--- src/shared/Modal/ui/Modal.tsx
+++ src/shared/Modal/ui/Modal.tsx
@@ -3,29 +3,32 @@ import {SyntheticEvent, useState} from "react";
 import {Box, Button, Tab, Tabs} from "@mui/material";
 
 import cls from "./Modal.module.scss";
-import {IPositionData, Position} from "@/shared/Position/ui/Position";
 import {List} from "@/shared/List";
 import {Block} from "@/shared/Block";
 import {Input} from "@/shared/Input";
 import {MultiBlock} from "@/shared/MultiBlock";
 import {PositionForm} from "@/shared/PositionForm";
+import {useDispatch, useSelector} from "react-redux";
+import {getList} from "@/shared/List/model/selectors/getList/getList";
+import {getPosition} from "@/shared/Position/model/selectors/getPosition/getPosition";
+import {listActions} from "@/shared/List/model/slice/ListSlice";
 interface ModalProps {
   className?: string
 }
 
 export const Modal = (props: ModalProps) => {
   const [value, setValue] = useState(0);
-  
+  const dispatch = useDispatch()
+  const selectedPosition = useSelector(getPosition)
+  const positionList = useSelector(getList)
+  console.log(positionList)
+  const addNewPosition = () => {
+    dispatch(listActions.addPosition())
+  }
   const handleChange = (event: SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
   
-  const positionsArray: IPositionData[] = [{
-    id: 11,
-    positionTitle: `Position`,
-    sumPerHour: 50,
-    otherInfo: `от ${10} lvl`
-  }];
   return (
     <div className={cls.mainContainer}>
       <div className={cls.tabsContainer}>
@@ -40,18 +43,27 @@ export const Modal = (props: ModalProps) => {
         {value === 1 && (
           <div className={cls.tabContent}>
             <div className={cls.leftSide}>
-              <List items={positionsArray} />
-              <Button className={cls.SaveButton}>Создать новую должность</Button>
+              <List items={positionList} />
+              <Button
+                className={cls.SaveButton}
+                onClick={addNewPosition}
+              >
+                Создать новую должность
+              </Button>
             </div>
-            <div className={cls.rightSide}>
-              <Block blockTitle="Название">
-                <Input />
-              </Block>
-              <MultiBlock title="Обязаности">
-                <PositionForm />
-              </MultiBlock>
-              <Button className={cls.SaveButton}>Сохранить</Button>
-            </div>
+              <div className={cls.rightSide}>
+            {selectedPosition && (
+              <>
+                <Block blockTitle="Название">
+                  <Input defaultValue={selectedPosition.positionTitle}/>
+                </Block>
+                <MultiBlock title="Обязаности">
+                  <PositionForm currentPositionData={selectedPosition}/>
+                </MultiBlock>
+                <Button className={cls.SaveButton}>Сохранить</Button>
+              </>
+            )}
+              </div>
           </div>
         )}
       </div>
